87067e87977f089ab458a56eb77eac0cadcb5a63
jarher <jefferh33@gmail.com>
2023-04-05 21:46:26 -0500

    creando el primer componente

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="/img/header.png" className="header" alt="header" />
+    </header>
+  );
+}
+
+export default Header;
